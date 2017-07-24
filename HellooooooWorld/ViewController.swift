ac31be1e0723ab01eaf7eb0da922e311ed29bc47
Larsen Eisenberg <magiclarsen@gmail.com>
2017-07-23 21:22:09 -0400

    Created a Hello World Application

--- HellooooooWorld/ViewController.swift
+++ HellooooooWorld/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var welcomeBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bgImageView.isHidden = true
+        logoImageView.isHidden = true
+        backBtn.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +29,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomeBtnWasPressed(_ sender: Any) {
+        bgImageView.isHidden = false
+        logoImageView.isHidden = false
+        welcomeBtn.isHidden = true
+        backBtn.isHidden = false
+    }
 
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        bgImageView.isHidden = true
+        logoImageView.isHidden = true
+        backBtn.isHidden = true
+        welcomeBtn.isHidden = false
+    }
 }
 
